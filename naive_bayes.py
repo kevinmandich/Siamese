f61ab65d06d51e456860eddfa71bc39e1ca12df4
Kevin Mandich <kevinmandich@gmail.com>
2015-01-30 22:37:22 -0800

    Created naive_bayes.py

---
+++ naive_bayes.py
@@ -0,0 +1,24 @@
+#from __future__ import division
+
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import numpy as np
+import pandas as pd
+import random as ran
+import operator as op
+import time
+
+
+class NaiveBayes(object):
+
+    def __init__(self, pandasData):
+
+        self.features = {}
+        
+
+
+
+
+if __name__ == '__main__':
+
+    data = sm.datasets.fair.load_pandas().data
